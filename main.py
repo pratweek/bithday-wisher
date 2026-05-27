193630726d221074a51343b5b60c6760aac6737e
Prateek Raj Singh <prateekrajsingh71@outlook.com>
2026-05-27 13:05:21 +0530

    Fix SMTP server address and improve dict comprehension

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
@@ -28,7 +28,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
